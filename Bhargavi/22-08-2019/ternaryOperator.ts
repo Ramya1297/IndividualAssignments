56e5dcb560dd262dcdf18b48b42022f6cd33d745
unknown <latabhargavi123@gmail.com>
2019-08-23 09:36:13 +0530

    angular 22nd assignment

---
+++ Bhargavi/22-08-2019/ternaryOperator.ts
@@ -0,0 +1,11 @@
+function ternary(child) {
+    return (child ? "$5.00" : "$10.00");
+  }
+  
+  console.log(ternary(true));
+
+  console.log(ternary(false));
+
+  console.log(ternary(1)); 
+
+  console.log(ternary(0));
